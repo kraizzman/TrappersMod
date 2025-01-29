b1d1db4eae45d116ac4b05fcb6e9ebd140ff76d5
kraizzman <JohanGagean.Pro@gmail.com>
2025-01-29 22:41:06 +0100

    Patching BlockEntityBasketTrapPatch to remove basketCatchable in IsSuitableFor

---
+++ TrappersMod/TrappersMod/Patches/BlockEntityBasketTrapPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.GameContent;
+
+namespace TrappersMod.Patches;
+
+[HarmonyPatch(typeof(BlockEntityBasketTrap))]
+public static class BlockEntityBasketTrapPatch
+{
+    [HarmonyPostfix, HarmonyPatch(nameof(BlockEntityBasketTrap.IsSuitableFor)), HarmonyPriority(Priority.Last)]
+    public static void IsSuitableFor(BlockEntityBasketTrap __instance, Entity entity, CreatureDiet diet, ref bool __result, ref InventoryGeneric ___inv)
+    {
+        if (__instance.TrapState == EnumTrapState.Ready && ___inv[0]?.Itemstack != null && diet != null && diet.Matches(___inv[0].Itemstack))
+        {
+            __result = true;
+        }
+    }
+}
--- TrappersMod/TrappersMod/TrappersModModSystem.cs
+++ TrappersMod/TrappersMod/TrappersModModSystem.cs
@@ -1,7 +1,13 @@
-﻿using Vintagestory.API.Client;
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using Newtonsoft.Json.Linq;
+using Vintagestory.API.Client;
 using Vintagestory.API.Server;
 using Vintagestory.API.Config;
 using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.Datastructures;
+using Vintagestory.GameContent;
 
 namespace TrappersMod;
 
@@ -11,16 +17,23 @@ public class TrappersModModSystem : ModSystem
     // Useful for registering block/entity classes on both sides
     public override void Start(ICoreAPI api)
     {
+        Harmony harmony = new Harmony("TrappersMod");
+        harmony.PatchAll();
         api.Logger.Notification("Hello from template mod: " + api.Side);
     }
 
     public override void StartServerSide(ICoreServerAPI api)
     {
-        api.Logger.Notification("Hello from template mod server side: " + Lang.Get("trappersmod:hello"));
+        api.Logger.Warning("Hello from template mod server side: " + Lang.Get("trappersmod:hello"));
     }
 
     public override void StartClientSide(ICoreClientAPI api)
     {
         api.Logger.Notification("Hello from template mod client side: " + Lang.Get("trappersmod:hello"));
+    }
+
+    public override bool ShouldLoad(EnumAppSide forSide)
+    {
+        return forSide == EnumAppSide.Client;
     }
 }
